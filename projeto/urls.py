894dc16dfc61c61ee5163c6f0bfe3b335642ad16
Adriano Luiz Rodrigues <rRodrigues.l.adriano@gmail.com>
2022-11-26 11:36:06 -0300

    Commit com overview inicial sobre URLs e views.

--- projeto/urls.py
+++ projeto/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+# HTTP REQUEST
+
+
+def my_view(request):
+    return HttpResponse('Uma string qualquer')
+    # return HTTP Response
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
