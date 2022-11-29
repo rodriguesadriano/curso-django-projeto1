384f22212a39236696070f24def77c5455244376
Adriano Luiz Rodrigues <rRodrigues.l.adriano@gmail.com>
2022-11-29 16:58:43 -0300

    Inicio da criação da grid

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,7 @@
-from django.http import HttpResponse
-
-# from django.shortcuts import render
+from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Home')
-
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
+    return render(request, 'recipes/pages/home.html')
