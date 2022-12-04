5c8dee1c1f98919d6b95c8b833ea497f9a9eaea8
Adriano Luiz Rodrigues <rRodrigues.l.adriano@gmail.com>
2022-12-04 14:19:03 -0300

    Criando usuarios

--- recipes/views.py
+++ recipes/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
 
 from recipes.models import Recipe
-from utils.recipes.factory import make_recipe
+
+# from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    recipes = Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
 
 def recipe(request, id):
+    recipes = Recipe.objects.get(id=id)
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipes,
         'is_detail_page': True,
     })
