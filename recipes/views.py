944b067777522c84d0704b531fe79e14101dc1de
Adriano Luiz Rodrigues <rRodrigues.l.adriano@gmail.com>
2022-12-05 11:11:05 -0300

    Shortcut get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from recipes.models import Recipe
 
@@ -15,10 +15,12 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
